f4f981be8edea92a504231f63b320f6a20be9b76
nitesh kumawat <kumawatnitjee@gmail.com>
2023-04-15 22:12:07 +0530

    adding script for heroku

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoDB=require('./db')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000 ;
 
 app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
